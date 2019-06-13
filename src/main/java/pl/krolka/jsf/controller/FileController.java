0fff1d8ac71816621bb5b4da9c9e7c29c7b88f06
Krzysztof Rolka <krzysztofrolka@op.pl>
2019-06-13 05:28:49 +0200

    Validate extention and size of file

--- src/main/java/pl/krolka/jsf/controller/FileController.java
+++ src/main/java/pl/krolka/jsf/controller/FileController.java
@@ -16,6 +16,7 @@ public class FileController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Part file;
+	
 
 	public FileController() {
 	}
---
+++ src/main/java/pl/krolka/jsf/validator/FileUploadValidator.java
@@ -0,0 +1,48 @@
+package pl.krolka.jsf.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.servlet.http.Part;
+
+@FacesValidator(value = "fileUploadValidator")
+public class FileUploadValidator implements Validator<Object> {
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+		int properSizeInMB = 2;
+		String properExtention = "zip";
+
+		Part file = (Part) value;
+		FacesMessage message = null;
+
+		try {
+
+			if (file == null || file.getSize() <= 0 || file.getContentType().isEmpty()) {
+				message = new FacesMessage("Select a valid file");
+			}
+
+			else if (!file.getSubmittedFileName().endsWith(properExtention)) {
+				message = new FacesMessage("Select " + properExtention + " file");
+			}
+
+			else if (file.getSize() > (1000000 * properSizeInMB))
+				message = new FacesMessage(
+						"File size too big. File size allowed  is less than or equal to " + properSizeInMB + " MB.");
+
+			if (message != null && !message.getDetail().isEmpty()) {
+				message.setSeverity(FacesMessage.SEVERITY_ERROR);
+				throw new ValidatorException(message);
+			}
+
+		} catch (Exception ex) {
+			throw new ValidatorException(new FacesMessage(ex.getMessage()));
+		}
+
+	}
+
+}
